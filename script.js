581b20550151875d8c7e93cbefe6e5ae1660a231
OldGregg32 <wordstream@carolina.rr.com>
2023-12-03 21:07:07 -0500

    Update to new version

--- script.js
+++ script.js
@@ -1,57 +1,44 @@
 
-function rotEncrypt(message){
-  let rotNum = parseInt(document.getElementById("rot_num").value);
-    if(rotNum < -26 || rotNum > 26){
-      return 'Rot number is either too low or too high. Max of 26, min of -26.';
-    }
-    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    return message.split('').map((x) => {
-      if(alphabet.includes(x.toLowerCase())){
-        if(x.toLowerCase()===x){
-          return alphabet[alphabet.indexOf(x)+rotNum>=26?alphabet.indexOf(x)+rotNum-26:alphabet.indexOf(x)+rotNum]
-        } else {
-          return alphabet[alphabet.indexOf(x.toLowerCase())+rotNum>=26?alphabet.indexOf(x.toLowerCase())+rotNum-26:alphabet.indexOf(x.toLowerCase())+rotNum].toUpperCase();
-        }
+function rot13(message){
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  return message.split('').map((x) => {
+    if(alphabet.includes(x.toLowerCase())){
+      if(x.toLowerCase()===x){
+        return alphabet[alphabet.indexOf(x)+13>=26?alphabet.indexOf(x)+13-26:alphabet.indexOf(x)+13]
       } else {
-        return x;
+        return alphabet[alphabet.indexOf(x.toLowerCase())+13>=26?alphabet.indexOf(x.toLowerCase())+13-26:alphabet.indexOf(x.toLowerCase())+13].toUpperCase();
       }
-    }).join('');
-  }
-
-
-  function rotDecrypt(message){
-    let rotNum = parseInt(document.getElementById("rot_num").value);
-    if(rotNum < -26 || rotNum > 26){
-      return 'Rot number is either too low or too high. Max of 26, min of -26.';
+    } else {
+      return x;
     }
-    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-    return message.split('').map((x) => {
-      if(alphabet.includes(x.toLowerCase())){
-        if(x.toLowerCase()===x){
-          return alphabet[alphabet.indexOf(x)-rotNum<0?26-(rotNum-(alphabet.indexOf(x))):alphabet.indexOf(x)-rotNum]
-        } else {
-          return alphabet[alphabet.indexOf(x.toLowerCase())+rotNum>=26?alphabet.indexOf(x.toLowerCase())+rotNum-26:alphabet.indexOf(x.toLowerCase())+rotNum].toUpperCase();
-        }
-      } else {
-        return x;
-      }
-    }).join('');
-  }
+  }).join('');
+}
 
   document.getElementById("result_button").addEventListener("click", ()=> {
-    if(document.getElementById('encrypt').checked){
-      document.getElementById("output_box").innerHTML = rotEncrypt(document.getElementById("text_input").value)
-    } else {
-      document.getElementById("output_box").innerHTML = rotDecrypt(document.getElementById("text_input").value)
-    }
+    document.getElementById("output_box").innerHTML = rot13(document.getElementById("text_input").value)
   });
   
   document.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-      if(document.getElementById('encrypt').checked){
-        document.getElementById("output_box").innerHTML = rotEncrypt(document.getElementById("text_input").value)
-      } else {
-        document.getElementById("output_box").innerHTML = rotDecrypt(document.getElementById("text_input").value)
-      }
+      document.getElementById("output_box").innerHTML = rot13(document.getElementById("text_input").value)
     }
+  });
+
+
+  function downloadFunc(textContent) {  
+    const blob = new Blob([textContent], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+  
+    const downloadLink = document.createElement('a');
+    downloadLink.href = url;
+    downloadLink.download = `rot13.txt`;
+  
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+    URL.revokeObjectURL(url);
+  }
+
+  document.getElementById("download_button").addEventListener("click", ()=>{
+    downloadFunc(document.getElementById("output_box").value);
   });
